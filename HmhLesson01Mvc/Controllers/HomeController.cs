e21438cae7fd652584be00643813eb7bf769e3f6
Hoàng Mạnh Huy <huy01255847658@gmail.com>
2026-08-19 16:21:52 +0700

    HmhLesson01

---
+++ HmhLesson01Mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using HmhLesson01Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HmhLesson01Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.name = "Hoàng M?nh Huy";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HmhLessonn01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HmhLessonn01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello! Hoàng Mạnh Huy");
+
+            app.Run();
+        }
+    }
+}
