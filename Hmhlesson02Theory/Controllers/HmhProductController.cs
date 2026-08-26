a909c18dd97c1063749df544a76af6ba62a0fb86
Hoàng Mạnh Huy <huy01255847658@gmail.com>
2026-08-26 16:18:38 +0700

    HmhLesson02

---
+++ Hmhlesson02Theory/Controllers/HmhProductController.cs
@@ -0,0 +1,35 @@
+﻿using Hmhlesson02Theory.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hmhlesson02Theory.Controllers
+{
+    public class HmhProductController : Controller
+    {
+        public IActionResult HmhIndex()
+        {
+            //dữ liệu lưu trong đối tượng ViewBag, ViewData, TempData
+            ViewBag.Name = "Hoàng Mạnh Huy";
+            ViewData["productVD"] = "LapTop Dell Vostro";
+            TempData["UNI"] = "Trường Đại Học Nguyễn Trãi - NTU";
+
+            return View();
+        }
+
+        public IActionResult GetProduct()
+        {
+            //Tạo mock data  product
+            HmhProduct hmhProduct = new HmhProduct()
+            {
+                ProductID = "2410900040",
+                ProductName = "Hoàng Mạnh Huy",
+                YearRelease = 1979,
+                Price = 1000
+            };
+
+            ViewBag.Product = hmhProduct;
+            ViewData["Product"] = hmhProduct;
+
+            return View("Product");
+        }
+    }
+}
---
+++ Hmhlesson02Theory/Models/HmhProduct.cs
@@ -0,0 +1,12 @@
+﻿namespace Hmhlesson02Theory.Models
+{
+    public class HmhProduct
+    {
+        public string ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public int YearRelease { get; set; }
+        public decimal Price { get; set; }
+    }
+}
